0dc7e5ce76747e663a6a53adc16b4c301b6f1d10
VoiiDz <moncifjb@gmail.com>
2021-02-12 20:02:56 +0100

    add the initial Rows

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const APIKEY = "bc692cd391a5bfed9f97125c62584f92";
+
+const requests = {
+    getTrending: `/trending/all/day?api_key=${APIKEY}&language=en-US`,
+    getNetflixOri: `/discover/movie?api_key=${APIKEY}&with_networks=213`,
+    getTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    getActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    getComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    getHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    getRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=99`
+}
+
+export default requests;
