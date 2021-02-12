147c3167be18c738e26bb866b3b83844d3345f01
VoiiDz <moncifjb@gmail.com>
2021-02-12 21:55:51 +0100

    app and row clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Row from "./Row";
 import requests from "./requests";
 
@@ -8,6 +7,10 @@ function App() {
       <h1>Hello</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.getNetflixOri} />
       <Row title="Trending Now" fetchUrl={requests.getTrending} />
+      <Row title="Action Movies" fetchUrl={requests.getActionMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.getRomanceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.getHorrorMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.getHorrorMovies} />
     </div>
   );
 }
