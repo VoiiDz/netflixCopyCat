80b8ee34943fbfbeb7dad78e32d7e8851bb07d42
VoiiDz <moncifjb@gmail.com>
2021-02-14 20:29:35 +0100

    finish up the Nav component

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css';
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        })
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, [])
+
+    return (
+        <div className={`nav ${show && "nav-black"}`}>
+            <img 
+                className="nav-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" 
+                alt="Netflix Logo"
+            />
+            <img 
+                className="nav-avatar"
+                src="../ninja.svg" 
+                alt="Netflix Avatar"
+            />
+        </div>
+    )
+}
+
+export default Nav
